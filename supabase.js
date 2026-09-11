e6d3691b2e0e9034dc936e7cb3b994a0e20b9597
shahinur-C6b3r <151050566+ciber846@users.noreply.github.com>
2026-09-11 21:37:33 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
 SUPABASE_URL=https://bozuqgvzlonzbwqiujzo.supabase.co
 SUPABASE_PUBLISHABLE_KEY=sb_publishable_X_yRnrd_3dVTI0k9LFDWtQ_c_SiACjs
-SUPABASE_SECRET_KEY=sb_secret_H273f-kzSOBdKR_81QzPVA_w5F7rrFW
+SUPABASE_SECRET_KEY=your-secret-key
 SUPABASE_JWKS_URL=https://bozuqgvzlonzbwqiujzo.supabase.co/auth/v1/.well-known/jwks.json
